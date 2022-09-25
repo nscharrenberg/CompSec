b26a1fe85b8de9dea4809c2f1655a6e1882d42d4
Dikaver <dika1596@gmail.com>
2022-09-25 21:31:51 +0200

    client/users

--- main_user.py
+++ main_user.py
@@ -1,7 +1,13 @@
 import user_c
 import client
 
-user = user_c.User("user1.json")
 
+users = []
 
-client.connect_to_server(user)
+max = 10
+
+for i in range(0, max):
+    users.append(user_c.User("json_files/user" + i + ".json"))
+
+for user in users:
+    client.connect_to_server(user)
